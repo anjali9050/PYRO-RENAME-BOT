a9e196a541b0415089299afe67007502a3fe9351
jakharamit44 <125469531+jakharamit44@users.noreply.github.com>
2023-10-15 17:11:13 +0530

    Update file_rename.py

--- plugins/file_rename.py
+++ plugins/file_rename.py
@@ -19,24 +19,24 @@ async def rename_start(client, message):
   file = getattr(message, message.media.value)
   filename = file.file_name  
   if file.file_size > 2000 * 1024 * 1024:
-     return await message.reply_text("Sᴏʀʀy Bʀᴏ Tʜɪꜱ Bᴏᴛ Iꜱ Dᴏᴇꜱɴ'ᴛ Sᴜᴩᴩᴏʀᴛ Uᴩʟᴏᴀᴅɪɴɢ Fɪʟᴇꜱ Bɪɢɢᴇʀ Tʜᴀɴ 2Gʙ")
+     return await message.reply_text("Sorry Bro This Bot Is Doesn't Support Uploading Files Bigger Than 2GB")
 
-  try:
-    await message.reply_text(
-      text=f"**__Pʟᴇᴀꜱᴇ Eɴᴛᴇʀ Nᴇᴡ Fɪʟᴇɴᴀᴍᴇ...__**\n\n**Oʟᴅ Fɪʟᴇ Nᴀᴍᴇ** :- `{filename}`",
-	  reply_to_message_id=message.id,  
-	  reply_markup=ForceReply(True)
-    )    
-    await sleep(30)
-  except FloodWait as e:
-    await sleep(e.value)
-    await message.reply_text(
-      text=f"**__Pʟᴇᴀꜱᴇ Eɴᴛᴇʀ Nᴇᴡ Fɪʟᴇɴᴀᴍᴇ...__**\n\n**Oʟᴅ Fɪʟᴇ Nᴀᴍᴇ** :- `{filename}`",
-	  reply_to_message_id=message.id,  
-	  reply_markup=ForceReply(True)
-    )
-  except:
-    pass
+  try:
+    await message.reply_text(
+      text=f"**__Please Enter New Filename...__**\n\n**Old File Name** :- `{filename}`",
+	  reply_to_message_id=message.id,  
+	  reply_markup=ForceReply(True)
+    )    
+    await sleep(30)
+  except FloodWait as e:
+    await sleep(e.value)
+    await message.reply_text(
+      text=f"**__Please Enter New Filename...__**\n\n**Old File Name** :- `{filename}`",
+	  reply_to_message_id=message.id,  
+	  reply_markup=ForceReply(True)
+    )
+  except:
+    pass
 @Client.on_message(filters.private & filters.reply)
 async def refunc(client, message):
   reply_message = message.reply_to_message
